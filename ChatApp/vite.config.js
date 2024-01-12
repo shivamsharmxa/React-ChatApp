44766bc778b50c359a593b6ab8e725ba8ceabecf
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-01-12 19:04:33 +0530

    Restore deleted and untracked files

--- ChatApp/vite.config.js
+++ ChatApp/vite.config.js
@@ -1,10 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build', // Specify your desired build folder name
+  resolve: {
+    alias: {
+      'websocket': resolve( 'node_modules/websocket'),
+    },
   },
 });
